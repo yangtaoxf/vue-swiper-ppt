114f0707bc0382494eda4fd6eee4f5619272cf47
王蕾蕾 <wangleilei@comedline.com>
2017-08-06 18:19:17 +0800

    主页面添加 hover 显示 左右切换图片的功能，并全屏之后原来的切换button。退出全屏按钮由原来的固定100px更改为top:10%;灰掉浏览器兼容的CSS代码,利用 autoprefixer 自动编译相应的代码。

--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -11,6 +11,7 @@
         display: flex;
         flex-direction: column;
         padding: 20px;
+        position: relative;
     }
 
     .mainBox img{
@@ -53,32 +54,32 @@
         display: none;
     }
 
-    #screenImg:fullscreen {
-        display: flex;
-        flex-direction: row;
-    }
-    #screenImg:fullscreen .icon{
-        display: flex;
-        align-items: center;
-        width: 60px;
-        background-color: black;
-    }
+    /*#screenImg:fullscreen {*/
+        /*display: flex;*/
+        /*flex-direction: row;*/
+    /*}*/
+    /*#screenImg:fullscreen .icon{*/
+        /*display: flex;*/
+        /*align-items: center;*/
+        /*width: 60px;*/
+        /*background-color: black;*/
+    /*}*/
 
-    #screenImg:fullscreen img {
-        display: flex;
-        justify-content: center;
-        width: 100%;
-        max-width: 1000px;
-        height: auto;
-    }
+    /*#screenImg:fullscreen img {*/
+        /*display: flex;*/
+        /*justify-content: center;*/
+        /*width: 100%;*/
+        /*max-width: 1000px;*/
+        /*height: auto;*/
+    /*}*/
 
-    #screenImg:fullscreen .icon-contract{
-        display: block;
-        position: fixed;
-        top: 100px;
-        right: 100px;
-        background-color: black;
-    }
+    /*#screenImg:fullscreen .icon-contract{*/
+        /*display: block;*/
+        /*position: fixed;*/
+        /*top: 100px;*/
+        /*right: 100px;*/
+        /*background-color: black;*/
+    /*}*/
 
     #screenImg:-webkit-full-screen {
         display: flex;
@@ -103,36 +104,55 @@
     #screenImg:-webkit-full-screen .icon-contract{
         display: block;
         position: absolute;
-        top: 100px;
-        right: 100px;
+        top: 10%;
+        right: 5%;
         background-color: black;
     }
 
-    #screenImg:-moz-full-screen {
-        display: flex;
-        flex-direction: row;
-    }
-    #screenImg:-moz-full-screen .icon{
-        display: flex;
-        align-items: center;
-        width: 60px;
-        background-color: black;
-    }
+    /*#screenImg:-moz-full-screen {*/
+        /*display: flex;*/
+        /*flex-direction: row;*/
+    /*}*/
+    /*#screenImg:-moz-full-screen .icon{*/
+        /*display: flex;*/
+        /*align-items: center;*/
+        /*width: 60px;*/
+        /*background-color: black;*/
+    /*}*/
 
-    #screenImg:-moz-full-screen img {
-        display: flex;
-        justify-content: center;
-        width: 100%;
-        max-width: 1000px;
-        height: auto;
-    }
+    /*#screenImg:-moz-full-screen img {*/
+        /*display: flex;*/
+        /*justify-content: center;*/
+        /*width: 100%;*/
+        /*max-width: 1000px;*/
+        /*height: auto;*/
+    /*}*/
 
-    #screenImg:-moz-full-screen .icon-contract{
+    /*#screenImg:-moz-full-screen .icon-contract{*/
+        /*display: block;*/
+        /*position: fixed;*/
+        /*top: 100px;*/
+        /*right: 100px;*/
+        /*background-color: black;*/
+    /*}*/
+
+    .icon-left {
+        display: none;
+        position: absolute;
+        bottom: 50%;
+        left: 5%;
+    }
+    .icon-right {
+        display: none;
+        position: absolute;
+        bottom: 50%;
+        right: 5%;
+    }
+    .mainBox:hover .icon-left {
         display: block;
-        position: fixed;
-        top: 100px;
-        right: 100px;
-        background-color: black;
+    }
+    .mainBox:hover .icon-right {
+        display: block;
     }
 
 </style>
@@ -143,21 +163,32 @@
                 <div class="icon-contract" @click="exitFullscreen()">
                     <a><Icon type="android-contract" style="font-size: 30px; color: #c3cbd6"></Icon></a>
                 </div>
-                <div class="icon">
-                   <a @click="changeImg(mark-1)" v-if="mark">
-                       <Icon type="chevron-left" style="font-size: 40px; color: #c3cbd6"></Icon>
-                   </a>
-                </div>
+                <!--<div class="icon">-->
+                   <!--<a @click="changeImg(mark-1)" v-if="mark">-->
+                       <!--<Icon type="chevron-left" style="font-size: 40px; color: #c3cbd6"></Icon>-->
+                   <!--</a>-->
+                <!--</div>-->
 
+                <div class="icon-left">
+                    <a @click="changeImg(mark-1)" v-if="mark">
+                        <Icon type="chevron-left" style="font-size: 40px; color: #c3cbd6"></Icon>
+                    </a>
+                </div>
                 <div v-for='(image, index) in images'>
                     <img :src="image"  alt="" :key="index" v-show="index === mark">
                 </div>
-
-                <div class="icon" style="padding-left: 30px">
+                <div class="icon-right">
                     <a @click="changeImg(mark+1)" v-if="images.length -1 - mark">
                         <Icon type="chevron-right" style="font-size: 40px; color: #c3cbd6 "></Icon>
                     </a>
                 </div>
+
+
+                <!--<div class="icon" style="padding-left: 30px">-->
+                    <!--<a @click="changeImg(mark+1)" v-if="images.length -1 - mark">-->
+                        <!--<Icon type="chevron-right" style="font-size: 40px; color: #c3cbd6 "></Icon>-->
+                    <!--</a>-->
+                <!--</div>-->
                 <img src="" alt="" v-show="this.images.length === 0">
             </div>
 
