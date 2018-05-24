5f51bcd637155cad16444f8181ca154457c143ce
王蕾蕾 <wangleilei@comedline.com>
2018-05-24 18:01:15 +0800

    修改失效的图片地址

--- src/components/index.vue
+++ src/components/index.vue
@@ -27,13 +27,11 @@ export default {
         return {
         msg: 'Welcome to Vue-swiper-ppt',
         images: [
-            "http://i4.bvimg.com/600318/dc9b2751fb7e091a.png",
-            "http://i4.bvimg.com/600318/e98d311c8df5136a.png",
-            "http://i4.bvimg.com/600318/c0546ad617bd4684.png",
-            "http://i4.bvimg.com/600318/38c5096a1374aa0e.png",
-            "http://i1.bvimg.com/600318/09bac0754e72ce98.png",
-            "http://i1.bvimg.com/600318/3331fb4c1ef78e94.png",
-            "http://i1.bvimg.com/600318/3498177c5d6a2e24.png",
+            'http://ow5nqfa5j.bkt.clouddn.com/imgPPT3.png',
+            'http://ow5nqfa5j.bkt.clouddn.com/imgPPT5.png',
+            'http://ow5nqfa5j.bkt.clouddn.com/imgPPT4.png',
+            'http://ow5nqfa5j.bkt.clouddn.com/imgPPT2.png',
+            'http://ow5nqfa5j.bkt.clouddn.com/imgPPT1.png'
         ],
     }
     },
