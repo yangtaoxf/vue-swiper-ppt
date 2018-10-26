70ec8d097318932236589c4e3037b206fdd1638d
王蕾蕾 <wangleilei01@zuoyebang.com>
2018-10-26 10:13:20 +0800

    edit
    1.代码优化
    2. 删除之前引用的iview icon，用图片代替
    3. 更换demo图片地址

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/index'
+import Index from '@/components/index'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Index',
+      component: Index
     }
   ]
 })
